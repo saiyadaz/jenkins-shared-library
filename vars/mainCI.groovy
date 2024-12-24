43339581eeaeb85bf1d231d0373118421ddfd7cd
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-12-24 13:21:22 -0500

    edit shared lib var jenkins

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -12,7 +12,7 @@ def call() {
             }
             checkout scmGit(
                     branches: [[name: "${branch_name}"]],
-                    userRemoteConfigs: [[url: "https://github.com/raghudevopsb78/roboshop-${component}"]]
+                    userRemoteConfigs: [[url: "https://github.com/saiyadaz/roboshop-${component}"]]
             )
         }
 
@@ -21,12 +21,13 @@ def call() {
                 sh 'docker build -t 633788536644.dkr.ecr.us-east-1.amazonaws.com/roboshop-${component}:${TAG_NAME} .'
             }
             stage('Release Software') {
-                sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 633788536644.dkr.ecr.us-east-1.amazonaws.com'
-                sh 'docker push 633788536644.dkr.ecr.us-east-1.amazonaws.com/roboshop-${component}:${TAG_NAME}'
+                sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 058264231458.dkr.ecr.us-east-1.amazonaws.com'
+                sh 'docker push 058264231458.dkr.ecr.us-east-1.amazonaws.com/roboshop-${component}:${TAG_NAME}'
+
             }
             stage('Deploy to Dev') {
                 sh 'aws eks update-kubeconfig --name dev-eks'
-                sh 'argocd login argocd-dev.rdevops6a.online --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web'
+                sh 'argocd login argocd-dev.devopssz14.online --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web'
                 sh 'argocd app set ${component} --parameter appVersion=${TAG_NAME}'
                 sh 'argocd app sync ${component}'
             }
